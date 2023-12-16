85f1392d1e49cce93a58e8b335130dbb79f75955
Ariayn <fatemiaryan@gmail.com>
2023-12-16 13:52:53 -0500

    last

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,9 +1,12 @@
+import Navbar from "./components/Navbar"
+import React from 'react';
+import ReactDOM from 'react-dom';
 export default function App() {
   return (
-    <div className="w-full justify-center">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar author="first one" text="first text"/>
+      <Navbar author="second one" text="second text"/>
+      <h1>hello</h1>
     </div>
   )
 }
---
+++ react-practice/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(props){
+    return(
+        <div className="justify-center text-zinc-900">
+            <h1>hello</h1>
+            <h1>{pops.author}</h1>
+            <p>{props.text}</p>
+        </div>
+    );
+
+}
