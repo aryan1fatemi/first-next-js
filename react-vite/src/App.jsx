a1936005cb5692c5d09edeba4118736391820f75
Ariayn <fatemiaryan@gmail.com>
2023-12-16 14:09:09 -0500

    new file

---
+++ react-vite/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-lg justify-center text-red-900">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
