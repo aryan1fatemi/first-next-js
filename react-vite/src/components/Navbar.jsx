e65a4c3458d753d67bd3a7a17e0a42c984f1bcff
Ariayn <fatemiaryan@gmail.com>
2023-12-20 15:33:23 -0500

    finished navbar

--- react-vite/src/components/Navbar.jsx
+++ react-vite/src/components/Navbar.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import { Link } from "react-scroll";
+import { IoMdCloseCircleOutline } from "react-icons/io";
+import { TbMenuDeep } from "react-icons/tb";
 
 export default function Navbar(){
+  const [click,setclick]=useState(false);
+  const handleClick=()=> setclick(!click);
   const content=
     <>
-      <div className="absolute ">
-        <ul>
-          <Link to="Home">
-            <li>Home</li>
+      <div className="lg:hidden block absolute top-16 w-full bg-slate-900 left-0 right-0 transition">
+        <ul className="text-center text-xl p-20">
+          <Link spy={true} smooth={true} to="Home">
+            <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Home</li>
           </Link >
-          <Link to="Projects">
-            <li>Projects</li>
+          <Link spy={true} smooth={true} to="Projects">
+            <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Projects</li>
           </Link>
-          <Link to="Contact">
-            <li>Contact</li>
+          <Link spy={true} smooth={true} to="Contact">
+            <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Contact</li>
           </Link>
         </ul>
       </div>
@@ -26,18 +31,26 @@ export default function Navbar(){
         <div className="lg:flex md:flex lg: flex-1 items-center justify-end font-normal hidden">
           <div className="flex-10">
             <ul className="flex gap-8 mr-16 text-[18px]">
-            <Link to="Home">
-              <li>Home</li>
+            <Link spy={true} smooth={true} to="Home">
+              <li className="hover:text-indigo-400 transition border-b-2 border-slate-900 cursor-pointer hover:border-indigo-400">Home</li>
             </Link >
-            <Link to="Projects">
-              <li>Projects</li>
+            <Link spy={true} smooth={true} to="Projects">
+              <li className="hover:text-indigo-400 transition border-b-2 border-slate-900 cursor-pointer hover:border-indigo-400">Projects</li>
             </Link>
-            <Link to="Contact">
-              <li>Contact</li>
+            <Link spy={true} smooth={true} to="Contact">
+              <li className="hover:text-indigo-400 transition border-b-2 border-slate-900 cursor-pointer hover:border-indigo-400">Contact</li>
             </Link>
             </ul>
           </div>
         </div>
+        <div>
+          {click&& content}
+        </div>
+
+        <button className="sm:hidden block transition " onClick={handleClick}>
+          {click? <IoMdCloseCircleOutline size={25}/> : <TbMenuDeep size={25}/>}
+        </button>
+
       </div>
     </nav>
     )
